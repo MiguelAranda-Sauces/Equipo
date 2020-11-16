30afcfd2f61429021587a5cc38b17eef014d5583
Rodrigmen <roblesminambresrodrigo@gmail.com>
2020-11-16 09:29:22 +0100

    Add rodrigoindex.php

---
+++ indexrodrigo.php
@@ -0,0 +1,9 @@
+<?php
+echo '<style>'
+. '*{'
+        . 'background-color: salmon;};'
+. '</style>';
+echo 'Este es el index del desarrollador Rodrigo Robles<br>';
+echo 'Enlace al index de Miguel Ángel (master): <a href="index.php">Enlace</a>';
+?>
+
